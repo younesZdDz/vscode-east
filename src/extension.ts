1a5bd166fae0da630030121569224ee5afb91a7d
younesZdDz <b.taamma@esi-sba.dz>
2021-01-13 12:34:52 +0100

    feat: xslt transformations to html

--- src/extension.ts
+++ src/extension.ts
@@ -2,19 +2,24 @@ import {
   commands,
   ExtensionContext,
   window,
+  Disposable,
+  workspace,
 } from "vscode";
 import {
   LanguageClientOptions,
   RevealOutputChannelOn,
   LanguageClient,
 } from "vscode-languageclient";
+import * as fs from "fs";
+import * as path from "path";
+import { xmlParse, xsltProcess } from "xslt-processor";
 import { prepareExecutable } from "./lemMinxStarter";
-  import * as requirements from "./requirements";
+import * as requirements from "./requirements";
 
 let languageClient: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-	return requirements
+  return requirements
     .resolveRequirements(context)
     .catch((error) => {
       window.showErrorMessage(error.message, error.label).then((selection) => {
@@ -27,8 +32,8 @@ export function activate(context: ExtensionContext) {
     .then((requirements) => {
       let clientOptions: LanguageClientOptions = {
         documentSelector: [
-          { scheme: "file", language: "xml" },
-          { scheme: "untitled", language: "xml" },
+          { scheme: "file", language: "xml", pattern: "**/*.east.xml" },
+          { scheme: "untitled", language: "xml", pattern: "**/*.east.xml" },
         ],
         revealOutputChannelOn: RevealOutputChannelOn.Never,
         initializationOptions: {
@@ -36,7 +41,7 @@ export function activate(context: ExtensionContext) {
             xml: {
               fileAssociations: [
                 {
-                  pattern: "**/*.xml",
+                  pattern: "**/*.east.xml",
                   systemId: "east.xsd",
                 },
               ],
@@ -64,7 +69,40 @@ export function activate(context: ExtensionContext) {
       let toDispose = context.subscriptions;
       let disposable = languageClient.start();
       toDispose.push(disposable);
-	});
+      return languageClient.onReady().then(() => {
+        const disposable: Disposable = commands.registerCommand(
+          "east.runXSLTTransform",
+          async (): Promise<any> => {
+            const xml: string = window.activeTextEditor.document.getText();
+            const xslt: string = fs
+              .readFileSync(workspace.rootPath + path.sep + "east.xslt")
+              .toString();
+            try {
+              const rXml = xmlParse(xml);
+              const rXslt = xmlParse(xslt);
+              const result = xsltProcess(rXml, rXslt);
+
+              // window.showTextDocument(textDoc, ViewColumn.Beside);
+
+              fs.writeFile(
+                workspace.rootPath + path.sep + "east.html",
+                result,
+                function (err) {
+                  if (err) {
+                    window.showErrorMessage("Error creating html file");
+                  }
+                }
+              );
+
+            } catch (e) {
+              window.showErrorMessage(e);
+            }
+          }
+        );
+
+        context.subscriptions.push(disposable);
+      });
+    });
 }
 
 export function deactivate() {}
